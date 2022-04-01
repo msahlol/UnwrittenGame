183e3364bbeee818ce2b7f7c06a4fdebe5915308
adrazilo <89820446+adrazilo@users.noreply.github.com>
2022-04-01 18:52:46 -0400

    Added in Story Triggers

---
+++ UnwrittenGame/Assets/Scripts/Decisions/StoryDecision.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StoryDecision : MonoBehaviour
+{
+    public string text;
+    public TextMeshProUGUI storyDisplay;
+
+    public GameObject wall;
+
+
+    private bool triggered = false;
+    void Start()
+    {
+        wall.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (!triggered)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                StartCoroutine(displayText());
+            }
+        }
+    }
+
+    private void OnTriggerExit()
+    {
+        if (!triggered)
+        {
+            wall.SetActive(true);
+            triggered = true;
+        }
+    }
+
+
+    private IEnumerator displayText()
+    {
+        if (storyDisplay.text != "")
+        {
+            text = " " + text;
+        }
+        storyDisplay.text += text;
+        WaitForSeconds wait = new WaitForSeconds(5.0f);
+        yield return wait;
+        storyDisplay.text = storyDisplay.text.Substring(text.Length, storyDisplay.text.Length-text.Length);
+        //storyDisplay.text = "";
+    }
+}
