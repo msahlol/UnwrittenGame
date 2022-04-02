b74c0ebb54d46a3fa2a63d972e8708d8b3455a58
adrazilo <89820446+adrazilo@users.noreply.github.com>
2022-04-01 21:35:05 -0400

    Added in Main Menu

---
+++ UnwrittenGame/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneName: "MainGame");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
